6f729940c6fcd54dc2728227976ba0d0305b6dc1
shashi priya mishra <shashipriyamishra@gmail.com>
2020-07-02 22:56:05 +0530

    update 8

--- index.java
+++ index.java
@@ -1,6 +1,6 @@
-public class index{
-
-     public static void main(String []args){
-        System.out.println("Hello World 2");
-     }
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
