48b09543e3cb08911234c814e3fcf981ecb8666d
shashi priya mishra <shashipriyamishra@gmail.com>
2020-07-02 17:02:35 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,10 @@
+public class HelloWorldExample{
+ 
+  public static void main(String args[]){
+ 
+    /*
+    Use System.out.println() to print on console.
+    */
+    System.out.println("Hello World !");
+  
+  }
